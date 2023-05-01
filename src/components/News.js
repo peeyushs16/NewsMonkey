4cf68cf223ab9ec45c62f3326210c9a9bc55d65a
peeyush <peeyush.sharma@formbay.com.au>
2023-05-01 16:19:54 +0530

    News with API Paging Menu

--- src/components/News.js
+++ src/components/News.js
@@ -1,57 +1,106 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import { Spinner } from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
 
-    article = [
-        {
-          "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-          "publishedAt": "2020-04-27T11:41:47Z",
-          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-          "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-          "publishedAt": "2020-03-30T15:26:05Z",
-          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-      ];
+    static defaultProps = {
+        country : 'in',
+        pageSize : 8,
+        category : 'general'
+    };
+
+    static propTypes = {
+        country : PropTypes.string,
+        pageSize : PropTypes.number,
+        category : PropTypes.string
+
+    };
 
     constructor(){
         super();
 
         this.state = {
-            article : this.article,
-            laoding : false
+            article : [],
+            laoding : false,
+            page : 1
         }
+    }
+
+    async componentDidMount() {
+        // fetch('https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=61d3210bd9214287a5d26cca956f220e')
+        // .then((response) => response.json())
+        // .then(articleList => {
+        //   console.log(articleList.articles);
+        //     this.setState({
+        //       article: articleList.articles,
+        //       laoding : false 
+        //     });
+        // });
+
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=61d3210bd9214287a5d26cca956f220e&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({laoding:true});
+        let articleList = await fetch(url);
+        let partDate = await articleList.json();
+        this.setState({
+              article: partDate.articles,
+              laoding : false ,
+              totalResults : partDate.totalResults
+        });
+
+    }
+
+    handlePrevClick = async () => {
+        console.log('prev');
+
+        
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=61d3210bd9214287a5d26cca956f220e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+            this.setState({laoding:true});
+            let articleList = await fetch(url);
+            let partDate = await articleList.json();
+            this.setState({
+                article: partDate.articles,
+                laoding : false ,
+                page : this.state.page - 1
+            });
+        
+
+        
+    }
+
+    handleNextClick = async () => {
+        console.log('next');
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=61d3210bd9214287a5d26cca956f220e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        this.setState({laoding:true});
+        let articleList = await fetch(url);
+        let partDate = await articleList.json();
+        this.setState({
+            article: partDate.articles,
+            laoding : false ,
+            page : this.state.page + 1
+        });        
     }
 
   render() {
     return (
         <>
             <div className="container my-3">
-                <h2>Newsmonkey - Top Headlines</h2>
+                <h2 className='text-center'>Newsmonkey - Top Headlines</h2>
+
+                {this.state.laoding && <Spinner />}
                 <div className="row my-3">
-                    {this.state.article.map((element)=>{
+                    {!this.state.laoding && this.state.article.map((element)=>{
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title.slice(0,72)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                            <NewsItem title={element.title.slice(0,72)} description={element.description!==null?element.description.slice(0,88):''} imageUrl={element.urlToImage} 
+                            newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                         </div>
                     })}
+                </div>
+
+                <div className="container d-flex justify-content-between">
+                    <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
                 </div>
             </div>
             
